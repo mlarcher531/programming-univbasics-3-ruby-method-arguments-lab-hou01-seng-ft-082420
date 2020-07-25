77e9bc1c5f1aa70298fa716850feac43dd8e72e7
Mike Archer <mlarcher531@gmail.com>
2020-07-25 16:44:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ end
 introduction me
 
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name}, and I am learning to code in #{language}"
+  puts "Hi, my name is #{name}, and I am learning to program in #{language}"
 end
 
 introduction_with_language("Mike", "Ruby")
